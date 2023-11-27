e6ec8553f7d7c5313bed6f6c99da94483c6923d9
Casey DeMattina <cdemattina@gmail.com>
2023-11-27 15:08:41 -0500

    clean up service image/text styles

--- app/components/ServiceItem.tsx
+++ app/components/ServiceItem.tsx
@@ -5,18 +5,18 @@ import { ServiceItemProps } from "../types";
 export default function Service(props: ServiceItemProps) {    
 
   return (
-    <Card className={styles.serviceCard}>
-      <div id={`service-${props.id}`} className="row service_wrapper">
+    <Card className={styles.service_card}>
+      <div id={`service-${props.id}`} className={`row ${styles.service_wrapper}`}>
         {/* title/description */}
-        <div className="col-sm service-text-wrapper">
-          <h2 className="service-title">{props.title}</h2>
-          <p className="service-description">{props.text}</p>
+        <div className={`col-sm ${styles.service_text_wrapper}`}>
+          <h2 className={styles.service_title}>{props.title}</h2>
+          <p className={styles.service_description}>{props.text}</p>
         </div>
 
         {/* image */}
-        <div className="col-sm">
+        <div className={`col-sm ${styles.service_image_wrapper}`}>
           <Image
-            className={styles.serviceImage}
+            className={styles.service_image}
             src={`/${props.image.src}`}
             width={props.image.width}
             height={props.image.height}
