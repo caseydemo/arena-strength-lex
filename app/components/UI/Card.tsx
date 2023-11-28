bd25c4e9c36bed762bcd1d60e765f7a39eaf19c9
Casey DeMattina <cdemattina@gmail.com>
2023-11-28 14:41:54 -0500

    add space between cards

--- app/components/UI/Card.tsx
+++ app/components/UI/Card.tsx
@@ -1,6 +1,8 @@
 import styles from "../../styles/card.module.css";
 export default function Card(props: any) {
-  const classes = `container card ${styles.arena_card} ` + (props.className ? props.className : "");  
+
+  // const classes = `container card ${styles.arena_card} ` + (props.className ? props.className : "");
+  const classes = `container card ${styles.arena_card} `;
   return (
     <div className={classes}>
       {props.children}
