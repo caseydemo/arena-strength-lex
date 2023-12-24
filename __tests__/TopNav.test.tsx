8bc0527092e65247476e6b51b7e55e2017e7b8b4
Casey DeMattina <cdemattina@gmail.com>
2023-12-24 12:17:47 -0500

    initial test commit

--- __tests__/TopNav.test.tsx
+++ __tests__/TopNav.test.tsx
@@ -1,9 +1,30 @@
 import { render, screen } from '@testing-library/react';
 import TopNav from '../app/components/TopNav';
+import { unlink } from 'fs';
 
 
 it('renders TopNav component', () => {
     render(<TopNav />); // ARRANGE
-    const linkElement = screen.getByText(/Open Gym/i); // ACT
-    expect(linkElement).toBeInTheDocument(); // ASSERT
+
+    // Open Gym
+    const open_gym_link = screen.getByText(/Open Gym/); // ACT
+    expect(open_gym_link).toBeInTheDocument(); // ASSERT
+    
+    // Group Classes
+    const group_classes_link = screen.getByText(/Group Classes/);
+    expect(group_classes_link).toBeInTheDocument();
+
+    // Personal Training
+    const personal_training_link = screen.getByText(/Personal Training/);
+    expect(personal_training_link).toBeInTheDocument();
+
+    // Rehab Only
+    const rehab_only_link = screen.getByText(/Rehab Only/);
+    expect(rehab_only_link).toBeInTheDocument();
+
+
+    // Contact Us
+    const contact_us_link = screen.getByText(/Contact Us/);
+    expect(contact_us_link).toBeInTheDocument();
+
 });
