f72cf4653256969c14a74bb3f8ab2bf09f52d821
Casey DeMattina <cdemattina@gmail.com>
2023-12-25 08:13:02 -0500

    add test - the anchor tags point to the right spots

--- __tests__/TopNav.test.tsx
+++ __tests__/TopNav.test.tsx
@@ -9,7 +9,7 @@ it('renders TopNav component', () => {
     // Open Gym
     const open_gym_link = screen.getByText(/Open Gym/); // ACT
     expect(open_gym_link).toBeInTheDocument(); // ASSERT
-    
+
     // Group Classes
     const group_classes_link = screen.getByText(/Group Classes/);
     expect(group_classes_link).toBeInTheDocument();
@@ -28,3 +28,23 @@ it('renders TopNav component', () => {
     expect(contact_us_link).toBeInTheDocument();
 
 });
+
+
+test("the anchor tags point to the right spots", () => {
+    render(<TopNav />);
+    
+    const open_gym_link = screen.getByRole('link', { name: 'Open Gym' });
+    expect(open_gym_link).toHaveAttribute('href', '#service-open-gym')
+    
+    const group_classes_link = screen.getByRole('link', { name: 'Group Classes' });
+    expect(group_classes_link).toHaveAttribute('href', '#service-group-classes')
+    
+    const personal_training_link = screen.getByRole('link', { name: 'Personal Training' });
+    expect(personal_training_link).toHaveAttribute('href', '#service-personal-training')
+    
+    const rehab_only_link = screen.getByRole('link', { name: 'Rehab Only' })
+    expect(rehab_only_link).toHaveAttribute('href', '#service-rehab-only')
+    
+    const contact_us_link = screen.getByRole('link', { name: 'Contact Us' });
+    expect(contact_us_link).toHaveAttribute('href', '#contact-us-form')
+})
