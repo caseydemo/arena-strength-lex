e4c158a77a9e532b7c76eaab2db577d663c9d97e
Casey DeMattina <cdemattina@ket.org>
2024-06-04 08:46:02 -0400

    reset card styling - starting from scratch

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,27 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import React from 'react';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import React from "react";
 
-
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Arena Strength & Performance',
-  description: 'Lexington Kentucky\'s premier weight training gym.',
-}
-
+    title: "Arena Strength & Performance",
+    description: "Lexington Kentucky's premier weight training gym.",
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className || ''}>
-        <div className="body-grid-item">
-          {children}                  
-        </div>
-      </body>
-    </html>
-  );
+    return (
+        <html lang='en'>
+            <body className={inter.className || ""}>
+                <div className='body-grid-item'>                    
+                    {children}
+                </div>
+            </body>
+        </html>
+    );
 }
