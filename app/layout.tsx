d159299542e43d4423fb94c9f44e28241bd9739a
Casey DeMattina <cdemattina@gmail.com>
2024-11-08 08:54:15 -0500

    add default seo to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,156 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import BootstrapClient from './components/UI/BootstrapClient'
-import React from 'react';
+import "bootstrap/dist/css/bootstrap.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import BootstrapClient from "./components/UI/BootstrapClient";
+import React from "react";
 
+const inter = Inter({ subsets: ["latin"] });
 
-
-const inter = Inter({ subsets: ['latin'] })
+const defaultImageUrl = "https://arena-strength-lex.com/bench_spot_1.png";
+const defaultSiteUrl = "https://arena-strength-lex.com/";
 
 export const metadata: Metadata = {
-  title: 'Arena Strength & Performance',
-  description: 'Lexington Kentucky\'s premier weight training gym.',
-}
+    title: "Arena Strength & Performance",
+    description:
+        "Lexington Kentucky's premier weight training gym. Powerlifting, Strongman, Personal Training, Group Classes and more.",
+    robots: "index, followw, max-image-preview:large, max-snippet:-1, max-video-preview:-1",
+    charSet: "UTF-8",
+    alternates: {
+        canonical: "https://arena-strength-lex.com/",
+    },
+    openGraph: {
+        title: "Arena Strength & Performance",
+        description:
+            "Lexington Kentucky's premier weight training gym. Powerlifting, Strongman, Personal Training, Group Classes and more.",
+        url: "https://arena-strength-lex.com/",
+        siteName: "Arena Strength & Performance",
+        images: defaultImageUrl,
+        locale: "en_US",
+    },
+};
 
+const defaultSchema = {
+    "@context": "https://schema.org",
+    "@graph": [
+        {
+            "@type": "WebPage",
+            "@id": defaultSiteUrl,
+            url: defaultSiteUrl,
+            name: "Arena Strength & Performance",
+            isPartOf: {
+                "@id": defaultSiteUrl + "#website",
+            },
+            about: {
+                "@id": defaultSiteUrl + "#organization",
+            },
+            primaryImageOfPage: {
+                "@id": defaultSiteUrl + "#primaryimage",
+            },
+            image: {
+                "@id": defaultSiteUrl + "#primaryimage",
+            },
+            thumbnailUrl: defaultImageUrl,
+            datePublished: "2016-05-06T19:27:44+00:00",
+            dateModified: "2023-05-10T16:47:04+00:00",
+            breadcrumb: {
+                "@id": defaultSiteUrl + "#breadcrumb",
+            },
+            inLanguage: "en-US",
+            potentialAction: [
+                {
+                    "@type": "ReadAction",
+                    target: [defaultSiteUrl,],
+                },
+            ],
+        },
+        {
+            "@type": "ImageObject",
+            inLanguage: "en-US",
+            "@id": defaultSiteUrl + "#primaryimage",
+            url: defaultImageUrl,
+            contentUrl: defaultImageUrl,
+            width: 1650,
+            height: 1096,
+            caption: "Arena Strength & Performance",
+        },
+        {
+            "@type": "BreadcrumbList",
+            "@id": defaultSiteUrl + "#breadcrumb",
+            itemListElement: [
+                {
+                    "@type": "ListItem",
+                    position: 1,
+                    name: "Home",
+                },
+            ],
+        },
+        {
+            "@type": "WebSite",
+            "@id": defaultSiteUrl + "#website",
+            url: defaultSiteUrl,
+            name: "Arena Strength & Performance",
+            description: "",
+            publisher: {
+                "@id": defaultSiteUrl + "#organization",
+            },
+            potentialAction: [
+                {
+                    "@type": "SearchAction",
+                    target: {
+                        "@type": "EntryPoint",
+                        urlTemplate:
+                            defaultSiteUrl + "?s={search_term_string}",
+                    },
+                    "query-input": {
+                        "@type": "PropertyValueSpecification",
+                        valueRequired: true,
+                        valueName: "search_term_string",
+                    },
+                },
+            ],
+            inLanguage: "en-US",
+        },
+        {
+            "@type": "Organization",
+            "@id": defaultSiteUrl + "#organization",
+            name: "Arena Strength & Performance",
+            url: defaultSiteUrl,
+            logo: {
+                "@type": "ImageObject",
+                inLanguage: "en-US",
+                "@id": defaultSiteUrl + "#/schema/logo/image/",
+                url: defaultImageUrl,
+                contentUrl:
+                    defaultImageUrl,
+                width: 250,
+                height: 160,
+                caption: "Arena Strength & Performance",
+            },
+            image: {
+                "@id": defaultSiteUrl + "#/schema/logo/image/",
+            },
+            sameAs: [
+                "https://www.facebook.com/profile.php?id=100088712705886",                
+            ],
+        },
+    ],
+};
+
+const schemaString = '<script type="application/ld+json">' + JSON.stringify(defaultSchema) + '</script>';
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className || ''}>
-        {children}
-        <BootstrapClient />
-      </body>
-    </html>
-  );
+    return (
+        <html lang='en'>
+            <body className={inter.className || ""}>
+            <div dangerouslySetInnerHTML={{ __html: schemaString }} />
+                {children}
+                <BootstrapClient />
+            </body>
+        </html>
+    );
 }
