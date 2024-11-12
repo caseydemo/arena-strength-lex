3e0584d23efd783f80beaffc13a6503f472be521
Casey DeMattina <cdemattina@gmail.com>
2024-11-12 12:42:53 -0500

    update tag id

--- app/layout.tsx
+++ app/layout.tsx
@@ -139,7 +139,8 @@ const defaultSchema = {
 };
 
 const schemaString = '<script type="application/ld+json">' + JSON.stringify(defaultSchema) + '</script>';
-const gtmId = "G-4QM0SQ0QYY";
+// const gtmId = "G-4QM0SQ0QYY";
+const gtmId = "GTM-K9DXK24W";
 const gtmUrl = "https://www.googletagmanager.com/gtag/js?id=" + gtmId;
 
 export default function RootLayout({
