a878e3bb9c696596fc0c75b4868289df1ffb6cc0
Casey DeMattina <cdemattina@gmail.com>
2025-01-04 14:51:41 -0500

    url failsafe string

--- app/components/ActionMenu.tsx
+++ app/components/ActionMenu.tsx
@@ -2,13 +2,14 @@ import ActionCard from "./UI/ActionCard";
 import Button from "./UI/Button";
 import styles from "../styles/action-menu.module.css";
 export default function ActionMenu(props: any) {
+	const url = process.env.JANE_APP_URL ? process.env.JANE_APP_URL : '';
   return (
     <div className={`container ${styles.action_menu_container}`}>
       <div className="row justify-content-center">
         <div className="col-md-3">
           <Button
             title="Get Started Today!"
-            url={process.env.JANE_APP_URL}
+            url={url}
             addClasses="btn-lg btn-primary sign-up-left"
           />
         </div>
