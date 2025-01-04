45682f57153f23f5572afc314b16023e3858dac9
Casey DeMattina <cdemattina@gmail.com>
2025-01-04 14:48:46 -0500

    remove action cards

--- app/components/ActionMenu.tsx
+++ app/components/ActionMenu.tsx
@@ -1,27 +1,23 @@
 import ActionCard from "./UI/ActionCard";
 import Button from "./UI/Button";
-import styles from "../styles/action-menu.module.css"
+import styles from "../styles/action-menu.module.css";
 export default function ActionMenu(props: any) {
   return (
     <div className={`container ${styles.action_menu_container}`}>
-      <div className="row justify-content-between">
-        <div className="col-md-6">
-          <ActionCard title="Get started today">
-            <Button
-              title="Get Started Today!"
-              url={process.env.JANE_APP_URL}
-              addClasses="btn-lg btn-primary sign-up-left"
-            />
-          </ActionCard>
+      <div className="row justify-content-center">
+        <div className="col-md-3">
+          <Button
+            title="Get Started Today!"
+            url={process.env.JANE_APP_URL}
+            addClasses="btn-lg btn-primary sign-up-left"
+          />
         </div>
-        <div className="col-md-6">
-          <ActionCard title="Have some questions?">
-            <Button
-              title="Contact Us"
-              url="#contact-us-form"
-              addClasses="btn-lg btn-secondary sign-up-right"
-            />
-          </ActionCard>
+        <div className="col-md-3">
+          <Button
+            title="Have some questions?"
+            url="#contact-us-form"
+            addClasses="btn-lg btn-secondary sign-up-right"
+          />
         </div>
       </div>
     </div>
