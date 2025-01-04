3238b3760e3d25f26c0e257549a212794fac789f
Casey DeMattina <cdemattina@gmail.com>
2025-01-04 12:09:03 -0500

    add basic styling to buttons

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,17 +1,27 @@
-import styles from '../styles/hero.module.css';
-import Button from './UI/Button';
+import styles from "../styles/hero.module.css";
+import Button from "./UI/Button";
 export default function Hero() {
-    console.log('jane app url:', process.env.JANE_APP_URL)
-    return (
-        <div className={styles.heroImage}>
-            <div className={styles.heroText}>
-                <h1 className={styles.heroTitle}>Arena Strength &amp; Performance</h1>
-                <p>Lexington KY&apos;s premiere weight training gym</p>
-            </div>
-            <div>
-                <Button title="Sign Up Now!" url={process.env.JANE_APP_URL} />
-                <Button title="Contact Us" url="#contact-us-form" />
-            </div>
+  console.log("jane app url:", process.env.JANE_APP_URL);
+  return (
+    <>
+      <div className={styles.heroImage}>
+        <div className={styles.heroText}>
+          <h1 className={styles.heroTitle}>Arena Strength &amp; Performance</h1>
+          <p>Lexington KY&apos;s premiere weight training gym</p>
+          <div>
+            <Button
+              title="Sign Up Now!"
+              url={process.env.JANE_APP_URL}
+              addClasses="btn-lg btn-primary sign-up-left"
+            />
+            <Button
+              title="Contact Us"
+              url="#contact-us-form"
+              addClasses="btn-lg btn-secondary sign-up-right"
+            />
+          </div>
         </div>
-    );
+      </div>
+    </>
+  );
 }
--- app/components/UI/Button.tsx
+++ app/components/UI/Button.tsx
@@ -1,14 +1,17 @@
 type ButtonProps = {
     title: string
     url: string
+    addClasses?: string
 }
+import "app/styles/button.css"
 import Link from "next/link";
 export default function Button(props: ButtonProps) {
     const title = props.title;
     const url = props.url;
+    const classes = props.addClasses ? 'btn ' + props.addClasses : 'btn'
     return (
         <Link href={url}>
-            <button >{title}</button>
+            <button className={classes} >{title}</button>
         </Link>
     );
 }
